9a3edd4a17020f4ba69d6df37c018c24f45c2701
Jahan Ara <jahan.ara@revinate.com>
2022-12-19 19:58:50 +0500

    Crud operations using dynamic data

--- src/main/java/com/api/book/bootrestbook/service/BookService.java
+++ src/main/java/com/api/book/bootrestbook/service/BookService.java
@@ -1,65 +1,104 @@
 package com.api.book.bootrestbook.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.api.book.bootrestbook.dao.BookRepository;
 import com.api.book.bootrestbook.entities.Book;
 
 @Component
 public class BookService {
 
-    private static List<Book> list = new ArrayList<>();
+    @Autowired
+    private BookRepository bookRepository;
 
-    static {
-        list.add(new Book(1, "Spring boot training", "xyz"));
-        list.add(new Book(2, "Spring boot training", "xyz"));
-        list.add(new Book(3, "Spring boot training", "xyz"));
-        list.add(new Book(4, "Spring boot training", "xyz"));
-        list.add(new Book(5, "Spring boot training", "xyz"));
+    // Static method
+    // private static List<Book> list = new ArrayList<>();
 
-    }
+    // static {
+    // list.add(new Book(1, "Spring boot training", "xyz"));
+    // list.add(new Book(2, "Spring boot training", "xyz"));
+    // list.add(new Book(3, "Spring boot training", "xyz"));
+    // list.add(new Book(4, "Spring boot training", "xyz"));
+    // list.add(new Book(5, "Spring boot training", "xyz"));
 
-    // Get all books
+    // }
+
+    // Get all books by static method
+    // public List<Book> getAllBooks() {
+    // return list;
+    // }
+
+    // Get all books by dynamic method
     public List<Book> getAllBooks() {
+        List<Book> list = (List<Book>) this.bookRepository.findAll();
         return list;
     }
 
-    // Get single book by id
+    // Get single book by id with static method
+    // public Book getBookById(int id) {
+    // Book book = null;
+    // try {
+
+    // book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+    // return book;
+    // }
+
+    // Get single book by id with dynamic method
     public Book getBookById(int id) {
         Book book = null;
         try {
 
-            book = list.stream().filter(e -> e.getId() == id).findFirst().get();
-            return book;
+            book = this.bookRepository.findById(id);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return book;
     }
 
-    // Add new book
+    // Add new book by static method
+    // public Book addBook(Book b) {
+    // list.add(b);
+    // return b;
+    // }
+
+    // Add new book by dynamic methos
     public Book addBook(Book b) {
-        list.add(b);
-        return b;
+        Book book = this.bookRepository.save(b);
+        return book;
     }
 
-    // Delete new book
+    // Delete new book static method
+    // public void deleteBook(int bId) {
+    // list = list.stream().filter(book -> book.getId() !=
+    // bId).collect(Collectors.toList());
+    // }
+
+    // Delete new book dynamic method
     public void deleteBook(int bId) {
-        list = list.stream().filter(book -> book.getId() != bId).collect(Collectors.toList());
+        bookRepository.deleteById(bId);
     }
 
-    // Update book
+    // Update book by static method
+    // public void updateBook(Book book, int bId) {
+    // list = list.stream().map(b -> {
+    // if (b.getId() == bId) {
+    // b.setAuthor(book.getAuthor());
+    // b.setTitle(book.getTitle());
+    // }
+    // return b;
+    // }).collect(Collectors.toList());
+    // }
+
+    // Update book by dynamic method
     public void updateBook(Book book, int bId) {
-        list = list.stream().map(b -> {
-            if (b.getId() == bId) {
-                b.setAuthor(book.getAuthor());
-                b.setTitle(book.getTitle());
-            }
-            return b;
-        }).collect(Collectors.toList());
+        book.setId(bId);
+        bookRepository.save(book);
     }
 
 }
