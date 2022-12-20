0496c81cce07d3c08e1be45ca950537c88e2c6bd
Jahan Ara <jahan.ara@revinate.com>
2022-12-20 14:02:57 +0500

    Unidirection relationship

--- src/main/java/com/api/book/bootrestbook/entities/Book.java
+++ src/main/java/com/api/book/bootrestbook/entities/Book.java
@@ -1,10 +1,12 @@
 package com.api.book.bootrestbook.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,7 +30,9 @@ public class Book {
     @Column(name="book_id")
     private int id;
     private String title;
-    private String author;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Author author;
 
 
     // public int getId() {
